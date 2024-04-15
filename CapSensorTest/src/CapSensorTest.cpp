2aa9f6b65329b09db8510bde495fb7b6da2f0dd8
Daniel Stromberg <dstromberg@mountain.local>
2024-04-15 15:05:36 -0600

    Lighting up LEDs when wires are touched.

--- CapSensorTest/src/CapSensorTest.cpp
+++ CapSensorTest/src/CapSensorTest.cpp
@@ -24,11 +24,17 @@ Adafruit_MPR121 cap = Adafruit_MPR121();
 
 int lastTouched = 0;
 int currTouched = 0;
+bool isLedOn[12];
+int ledPins[6] = {3,4,5,6,7,10};
 
 // setup() runs once, when the device is first turned on
 void setup() {
   Serial.begin(9600);
   cap.begin(0x5A);
+
+  for (int i=0; i<6; i++){
+    pinMode(ledPins[i], OUTPUT);
+  }
 }
 
 // loop() runs over and over again, as quickly as it can execute.
@@ -36,7 +42,20 @@ void loop() {
   currTouched = cap.touched();
   // Serial.printf("currtouched: %i\n", currTouched);
 
-
+  //Below does a bitwise and between the binary version of currTouched number
+  //  and 0-2048 in binary (starts 1 at 1's place, shifts left each loop).
+  //  when 'and'ed together we only get true when i is at the location of
+  //  a 1 in currTouched. 
+  //  33 dec =      000000100001    currTouched (in binary)
+  //  when i is 0:  000000000001    checking 1's place
+  //  'and'ed =     000000000001    there is a 1, 0 pin touched
+  //
+  // .... Increment i (move 1 to left to check other places) ....
+  //  
+  //  33 dec =      000000100001    currTouched (in binary)
+  //  i=5           000000100000    checking 32's place
+  //  'and'ed       000000100000    There is a 1, pin 5 touched!
+  //
   for (int i=0; i<12; i++){
     if((currTouched & _BV(i)) && !(lastTouched & _BV(i))){
       Serial.printf("%i Touched\n", i);
@@ -45,9 +64,16 @@ void loop() {
       Serial.printf("(currTouched & _BV(i)) = %i\n\n", currTouched & _BV(i));
     }
 
-    // if((currTouched & _BV(i) && (lastTouched & _BV(i)))){
-    //   Serial.printf("%i Released\n");
-    // }
+    if((currTouched & _BV(i) && (lastTouched & _BV(i)))){
+      // Serial.printf("%i Released\n");
+      isLedOn[i] = true;
+    } else{
+      isLedOn[i] = false;
+    }
+  }
+
+  for (int i=0; i<6; i++){
+    digitalWrite(ledPins[i], isLedOn[i]);
   }
 
   lastTouched = currTouched;
