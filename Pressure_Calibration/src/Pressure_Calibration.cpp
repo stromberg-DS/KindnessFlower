786ac8c2a172e4aa8a95b7aa69cadf3bf2cb7a1f
DStromberg <strombergundy@gmail.com>
2024-05-08 11:32:44 -0600

    Sensor arrays fill with data

--- Pressure_Calibration/src/Pressure_Calibration.cpp
+++ Pressure_Calibration/src/Pressure_Calibration.cpp
@@ -12,19 +12,45 @@
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 
-const int arraySize =10;
-int testArray[arraySize] = {4,8,3,9,8,7,8,8,3,4};
+const int SENSOR_COUNT = 6;
+const int SAMPLE_COUNT =10;
+const int PRESSURE_PINS[6] = {A0, A1, A2, A5, A4, A3};
+
+int pressureSamples [SENSOR_COUNT][SAMPLE_COUNT];
+int avgBaselines[SENSOR_COUNT];
+
+int testArray[SAMPLE_COUNT] = {4,8,3,9,8,7,8,8,3,4};
 
 float getArrayAverage(int array[], int size);
 
 void setup() {
-  delay(10000);
-  Serial.printf("Array Avg: %f\n", getArrayAverage(testArray, arraySize));
+  delay(5000);
+  Serial.printf("Array Avg: %f\n", getArrayAverage(testArray, SAMPLE_COUNT));
+
+  for(int i=0; i<SENSOR_COUNT; i++){
+      pinMode(PRESSURE_PINS[i], INPUT);
+  } 
 
 }
 
 void loop() {
+ static bool isSampling = true;
 
+  digitalWrite(D7, HIGH);
+
+  if (isSampling){
+    for(int i=0; i<SAMPLE_COUNT; i++){
+      Serial.printf("Sample #%i\n", i);
+      for(int j=0; j<SENSOR_COUNT; j++){
+        pressureSamples[j][i] = analogRead(PRESSURE_PINS[j]);
+        Serial.printf("   #%i: %i\n", j, pressureSamples[j][i]);
+      }
+      Serial.printf("\n");
+      
+    } 
+    digitalWrite(D7, LOW);
+    isSampling = false;
+  }
 
 }
 
