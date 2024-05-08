c8c4478bcfd563aa2478afc90088ba69400e0341
DStromberg <strombergundy@gmail.com>
2024-05-08 12:52:49 -0600

    Averaging one set of samples

--- Pressure_Calibration/src/Pressure_Calibration.cpp
+++ Pressure_Calibration/src/Pressure_Calibration.cpp
@@ -18,14 +18,18 @@ const int PRESSURE_PINS[6] = {A0, A1, A2, A5, A4, A3};
 
 int pressureSamples [SENSOR_COUNT][SAMPLE_COUNT];
 int avgBaselines[SENSOR_COUNT];
+bool isSampling = true;
 
-int testArray[SAMPLE_COUNT] = {4,8,3,9,8,7,8,8,3,4};
+int testArray[SAMPLE_COUNT] = {9,9,9,9,9,9,8,8,3,4};
 
-float getArrayAverage(int array[], int size);
+void getSensorSamples();
+int getArrayAverage(int array[], int size);
+int get2DArrayAvg(int array[SENSOR_COUNT][SAMPLE_COUNT], int currentPin);
+
 
 void setup() {
   delay(5000);
-  Serial.printf("Array Avg: %f\n", getArrayAverage(testArray, SAMPLE_COUNT));
+  Serial.printf("Array Avg: %i\n", getArrayAverage(testArray, SAMPLE_COUNT));
 
   for(int i=0; i<SENSOR_COUNT; i++){
       pinMode(PRESSURE_PINS[i], INPUT);
@@ -34,12 +38,25 @@ void setup() {
 }
 
 void loop() {
- static bool isSampling = true;
-
   digitalWrite(D7, HIGH);
 
   if (isSampling){
-    for(int i=0; i<SAMPLE_COUNT; i++){
+    getSensorSamples();
+    Serial.printf("\n\n");
+    avgBaselines[0] = get2DArrayAvg(pressureSamples, 0);
+    Serial.printf("Average of sensor 0: %i\n", avgBaselines[0]);
+  }
+  isSampling = false;
+  digitalWrite(D7, LOW);
+
+
+  // Serial.printf("Average of #0: %i\n", getArrayAverage(pressureSamples, SAMPLE_COUNT))
+
+}
+
+//Collect a bunch of samples from analog pins to be averaged later
+void getSensorSamples(){
+  for(int i=0; i<SAMPLE_COUNT; i++){
       Serial.printf("Sample #%i\n", i);
       for(int j=0; j<SENSOR_COUNT; j++){
         pressureSamples[j][i] = analogRead(PRESSURE_PINS[j]);
@@ -48,18 +65,30 @@ void loop() {
       Serial.printf("\n");
       
     } 
-    digitalWrite(D7, LOW);
-    isSampling = false;
-  }
-
+    
 }
 
-float getArrayAverage(int array[], int size){
+int get2DArrayAvg(int array[SENSOR_COUNT][SAMPLE_COUNT], int currentPin){
   int j;
-  float total = 0;
+  int average=0;
+  int total = 0;
+
+  for(j=0; j<SAMPLE_COUNT; j++){
+    total += array[currentPin][j];
+  }
+  average = int(total/SAMPLE_COUNT);
+  
+  return average;
+}
+
+int getArrayAverage(int array[], int size){
+  int j;
+  int average = 0;
+  int total = 0;
   for(j=0; j<size; j++){
     total += array[j];
   }
+  average = int(total/size);
 
-  return total/size;
+  return average;
 }
