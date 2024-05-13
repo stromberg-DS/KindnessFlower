c76c251d85b10d7b297667f2a5dafe1f80361279
DStromberg <strombergundy@gmail.com>
2024-05-13 12:22:36 -0600

    Created final project for flower code

---
+++ Kindness_Flower/src/Kindness_Flower.cpp
@@ -0,0 +1,110 @@
+/* 
+ * Kindness Flower
+ *      Portable flower that visitors take around the museum floor
+ *      with them. When they pass the flower to someone else, they
+ *      can light up the whole flower by grabbing all 4 pressure
+ *      sensors. We tally up the number of passes as a proxy for
+ *      kind acts. Future possibility of using BLE to find location
+ *      throughout the museum and activate parts of the museum as 
+ *      the flower gets closer.
+ * Author: Daniel Stromberg
+ * Date: 5/13/24
+ * 
+ * Use the Adafruit guide to set up FSRs from Adafruit (ID:1075)
+ *  https://learn.adafruit.com/force-sensitive-resistor-fsr/using-an-fsr
+ */
+
+#include "Particle.h"
+#include <neopixel.h>
+
+////////KEEEP AUTOMATIC///////////
+SYSTEM_MODE(AUTOMATIC);
+/////////////////////////////////
+SYSTEM_THREAD(ENABLED);
+
+const int SENSOR_COUNT = 6;
+const int PIXEL_COUNT = 144;
+const int PIXEL_ZONES[SENSOR_COUNT] = {24, 48, 72, 96, 120,144};
+const int PRESSURE_PINS[SENSOR_COUNT] = {A0, A1, A2, A5, A4, A3};
+const int gripStrength = 200;     //how hard to squeeze to trigger LEDs. Higher=harder squeeze
+int pressureBaselines[SENSOR_COUNT];
+
+//EEPROM Setup 
+int len = EEPROM.length();
+int baselineAddress = 0x0001;
+
+
+int brightness = 150;
+int pressureIn[SENSOR_COUNT];
+
+Adafruit_NeoPixel pixel(PIXEL_COUNT, SPI1, WS2812);
+
+//functions
+void ledStripStartup();
+void ledFill(int color, int firstLED = 0, int lastLED = PIXEL_COUNT);
+
+void setup() {
+    EEPROM.get(baselineAddress, pressureBaselines);
+
+
+    ledStripStartup();
+
+    Serial.printf("#### Incoming Average Baselines ####");
+
+    for(int i=0; i<6; i++){
+        pinMode(PRESSURE_PINS[i], INPUT);
+        Serial.printf("Last Avg #%i: %i\n",i , pressureBaselines[i]);
+    }
+    Serial.printf("\n\n");
+}
+
+void loop() {
+    for(int i=0; i<6; i++){
+        pressureIn[i] = analogRead(PRESSURE_PINS[i]);
+    }
+
+    for(int j=0; j<6; j++){
+        // Serial.printf("Pressure #%i: %i\n", j+1, pressureIn[j]);
+
+        if(pressureIn[j] - pressureBaselines[j] > gripStrength){
+            // Serial.printf("pressureIn: %i\nBaseline: %i\n\n", pressureIn[j], pressureBaselines[j]);
+            ledFill(0x00FF00, j*24, (j+1)*24);
+            // ledFill(0x00FF00);
+        } else{
+            ledFill(0, j*24, (j+1)*24);
+            // ledFill(0);
+        }
+    }
+    // Serial.printf("\n");    
+    delay(5);
+
+    
+
+    pixel.show();
+}
+
+void ledFill(int color, int firstLED, int lastLED) {
+    for (int i=firstLED; i<lastLED; i++){
+        pixel.setPixelColor(i, color);
+    }
+}
+
+void ledStripStartup(){
+    pixel.begin();
+    pixel.setBrightness(brightness);
+    pixel.clear();
+    pixel.show();
+
+    //Quick test of all LEDs to make sure the strip works. 
+    for (int i=0; i < PIXEL_COUNT; i++){
+        pixel.clear();
+        pixel.setPixelColor(i, 0xFFFFFF);
+        pixel.show();
+        delay(20);
+    }
+
+    delay(1000);
+
+    pixel.clear();
+    pixel.show();
+}
